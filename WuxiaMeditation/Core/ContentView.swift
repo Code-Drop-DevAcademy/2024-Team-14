f1c24c3000caf44572ae78fb1a14e911b0a6bbca
moonkey48 <moonkey48@icloud.com>
2024-06-15 01:32:40 +0900

    setting: set basic directory structure

---
+++ WuxiaMeditation/Core/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  WuxiaMeditation
+//
+//  Created by Austin's Macbook Pro M3 on 6/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hello world")
+    }
+}
+
+#Preview {
+    ContentView()
+}
