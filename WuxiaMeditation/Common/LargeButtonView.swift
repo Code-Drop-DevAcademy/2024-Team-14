dfda86ed3f6177e29f638488b29c9a9904198b13
moonkey48 <moonkey48@icloud.com>
2024-06-15 01:42:14 +0900

    design: LargeButtonView 구현 및 Common directory 생성

---
+++ WuxiaMeditation/Common/LargeButtonView.swift
@@ -0,0 +1,34 @@
+//
+//  LargeButtonView.swift
+//  WuxiaMeditation
+//
+//  Created by Austin's Macbook Pro M3 on 6/15/24.
+//
+
+import SwiftUI
+
+struct LargeButtonView: View {
+    var title: String
+    var color: Color = .init(.white)
+    var completion: () -> Void
+    
+    var body: some View {
+        Button {
+            completion()
+        } label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 12.0)
+                    .fill(.primaryGreen)
+                    .frame(width: .infinity, height: 50)
+                Text(title)
+                    .foregroundStyle(color)
+            }
+        }
+    }
+}
+
+#Preview {
+    LargeButtonView(title: "시작") {
+        print("start")
+    }
+}
---
+++ WuxiaMeditation/View/OnboardingView.swift
@@ -0,0 +1,22 @@
+//
+//  OnboardingView.swift
+//  WuxiaMeditation
+//
+//  Created by Austin's Macbook Pro M3 on 6/15/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        VStack {
+            LargeButtonView(title: "시작") {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
