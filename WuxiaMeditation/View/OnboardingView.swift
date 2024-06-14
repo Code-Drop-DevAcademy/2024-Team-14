9dc8efe9cdc6095e5e60f6f1f8ecd361985590c6
moonkey48 <moonkey48@icloud.com>
2024-06-15 01:49:51 +0900

    design: OnboardingView 구현 및 isOnboarding UserDefault 설정

--- WuxiaMeditation/View/OnboardingView.swift
+++ WuxiaMeditation/View/OnboardingView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @AppStorage("isOnboarding") private var isOnboarding: Bool = true
+    
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
+            Spacer()
+            Text("運氣調息")
+                .foregroundStyle(.white)
+                .font(.system(size: 34))
+                .padding(.bottom, 16)
+            Text("일과 삶의 마음 지킴 명상앱\n운기조식運氣調息")
+                .foregroundStyle(.white)
+                .multilineTextAlignment(.center)
+                .font(.system(size: 13))
+                .lineSpacing(6)
+            Spacer()
             LargeButtonView(title: "시작") {
-                
+                isOnboarding = true
             }
         }
+        .background(
+            Image(.background)
+                .edgesIgnoringSafeArea(.top)
+        )
+        .padding(16)
+        
     }
 }
 
